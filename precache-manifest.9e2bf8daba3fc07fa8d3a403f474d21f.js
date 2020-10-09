7eb4e31ad685ead2acae41d697ecb10ff4aaddaf
i1-kmv <i1-kmv@mail.ru>
2020-10-09 14:34:30 +0200

    Updates

---
+++ precache-manifest.9e2bf8daba3fc07fa8d3a403f474d21f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a21dfa83d6da5020c84aba286e27f6",
+    "url": "/TODOLIST-TS/index.html"
+  },
+  {
+    "revision": "6239a1854b93ec27247d",
+    "url": "/TODOLIST-TS/static/css/main.6da83bb5.chunk.css"
+  },
+  {
+    "revision": "6386a6738ade9692f755",
+    "url": "/TODOLIST-TS/static/js/2.0aa5ce28.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TODOLIST-TS/static/js/2.0aa5ce28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6239a1854b93ec27247d",
+    "url": "/TODOLIST-TS/static/js/main.8acaa31c.chunk.js"
+  },
+  {
+    "revision": "678122e7eae71a095528",
+    "url": "/TODOLIST-TS/static/js/runtime-main.772af540.js"
+  }
+]);
